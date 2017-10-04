1892fb8dd4be6ba83892a573f07712532ddedcd4
turtleDev <saravjeetamansingh@gmail.com>
2017-10-04 12:54:50 +0530

    add dependencies

--- main.go
+++ main.go
@@ -2,12 +2,24 @@ package main
 
 import (
     "fmt"
+    "time"
+    "github.com/tj/go-spin"
 )
 
 func foo() string {
     return "bar"
 }
 
+func compute() {
+    s := spin.New()
+    for i := 0; i < 100; i++ {
+        fmt.Printf("\r%s", s.Next())
+        time.Sleep(100 * time.Millisecond)
+    }
+
+}
+
 func main() {
+    compute()
     fmt.Println(foo())
 }
